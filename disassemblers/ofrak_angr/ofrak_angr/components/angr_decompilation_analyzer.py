53453d46b0857bc498559348a2c712be8bfaecea
Dan Pesce <dan@redballoonsecurity.com>
2024-02-23 14:34:34 -0500

    some better error handling

--- disassemblers/ofrak_angr/ofrak_angr/components/angr_decompilation_analyzer.py
+++ disassemblers/ofrak_angr/ofrak_angr/components/angr_decompilation_analyzer.py
@@ -31,34 +31,37 @@ class AngrDecompilatonAnalyzer(Analyzer[None, AngrDecompilationAnalysis]):
 
     async def analyze(self, resource: Resource, config: None) -> AngrDecompilationAnalysis:
         # Run / fetch angr analyzer
-        root_resource = await resource.get_only_ancestor(
-            ResourceFilter(tags=[AngrAnalysisResource], include_self=True)
-        )
-        complex_block = await resource.view_as(ComplexBlock)
-        angr_analysis = await root_resource.analyze(AngrAnalysis)
+        try:
+            root_resource = await resource.get_only_ancestor(
+                ResourceFilter(tags=[AngrAnalysisResource], include_self=True)
+            )
+            complex_block = await resource.view_as(ComplexBlock)
+            angr_analysis = await root_resource.analyze(AngrAnalysis)
 
-        cfg = angr_analysis.project.analyses[angr.analyses.CFGFast].prep()(
-            data_references=True, normalize=True
-        )
+            cfg = angr_analysis.project.analyses[angr.analyses.CFGFast].prep()(
+                data_references=True, normalize=True
+            )
 
-        function_s = [
-            func
-            for addr, func in angr_analysis.project.kb.functions.items()
-            if func.addr == complex_block.virtual_address
-        ]
-        if len(function_s) != 1:
-            # Check for thumb
             function_s = [
                 func
                 for addr, func in angr_analysis.project.kb.functions.items()
-                if func.addr == complex_block.virtual_address + 1
+                if func.addr == complex_block.virtual_address
             ]
-        if len(function_s) != 1:
-            raise ValueError(
-                f"Could not find angr function for function at address {complex_block.virtual_address}"
+            if len(function_s) != 1:
+                # Check for thumb
+                function_s = [
+                    func
+                    for addr, func in angr_analysis.project.kb.functions.items()
+                    if func.addr == complex_block.virtual_address + 1
+                ]
+            if len(function_s) != 1:
+                raise ValueError(
+                    f"Could not find angr function for function at address {complex_block.virtual_address}"
+                )
+            function = function_s[0]
+            dec: Decompiler = angr_analysis.project.analyses[angr.analyses.Decompiler].prep()(
+                function, cfg=cfg.model, options=None
             )
-        function = function_s[0]
-        dec: Decompiler = angr_analysis.project.analyses[angr.analyses.Decompiler].prep()(
-            function, cfg=cfg.model, options=None
-        )
-        return AngrDecompilationAnalysis(dec.codegen.text)
+            return AngrDecompilationAnalysis(dec.codegen.text)
+        except Exception as e:
+            return AngrDecompilationAnalysis(f"The decompilation for this Complex Block has failed with the error {e}")
